50e92e763fd6f012d213b819d50b817c4790a03d
Duy Tung <dtung124@gmail.com>
2022-10-12 00:38:20 +0700

    Su dung toan tu

---
+++ Biến, kiểu dữ liệu và toán tử/operator/operator.js
@@ -0,0 +1,6 @@
+let inputWidth = prompt("Nhập chiều dài: ");
+let inputHeight = prompt("Nhập chiều rộng: ");
+let Width = parseInt(inputWidth);
+let Height = parseInt(inputHeight);
+let area = inputWidth * inputHeight;
+document.write("The area is: " + area);
