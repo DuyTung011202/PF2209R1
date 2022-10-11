7a6e7d3d639aa84b29a3050f39eaf06ceebf2c73
Duy Tung <dtung124@gmail.com>
2022-10-12 00:34:47 +0700

    Hien thi ngay thang nam nhap vao

---
+++ Biến, kiểu dữ liệu và toán tử/date/date.js
@@ -0,0 +1,4 @@
+let inputDay = prompt("Nhập vào ngày: ")
+let inputMonth = prompt("Nhập vào tháng: ")
+let inputYear = prompt("Nhập vào năm")
+document.write(inputDay + " - " + inputMonth + " - " + inputYear)   
