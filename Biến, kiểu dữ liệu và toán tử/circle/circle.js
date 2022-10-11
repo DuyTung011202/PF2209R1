91bf4a8b34e263d9001326f448b30bdf30f6ee2e
Duy Tung <dtung124@gmail.com>
2022-10-12 00:34:22 +0700

    tinh chu vi va dien tich hinh tron

---
+++ Biến, kiểu dữ liệu và toán tử/circle/circle.js
@@ -0,0 +1,11 @@
+let r = prompt("Nhập bán kính của hình tròn: ")
+let d = 2 * r
+let C = 2 * Math.PI * r
+let S = Math.PI * r * r
+document.write("Đường kính của hình tròn: " + d)
+document.write("<br>")
+document.write("Chu vi của hình tròn: " + C)
+document.write("<br>")
+document.write("Diện tích của hình tròn: " + S)
+
+
