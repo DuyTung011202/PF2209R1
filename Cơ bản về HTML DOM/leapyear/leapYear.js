4e9740fe564b9b78fd5858734ebd728d7a31989e
Duy Tung <dtung124@gmail.com>
2022-10-18 21:01:31 +0700

    Kiem tra nam nhuan

--- Cơ bản về HTML DOM/leapyear/leapYear.js
+++ Cơ bản về HTML DOM/leapyear/leapYear.js
@@ -5,6 +5,7 @@ let inputYear = prompt("Nhập năm: ")
 if(inputYear % 4 ==0 && inputYear % 100 != 0 || inputYear % 400 ==0) {
     alert(inputYear + " is leap year")
 }
+
 else {
     alert(inputYear + " is not leap year")
 }
